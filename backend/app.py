340663a61adc37f85254fb9fcb870fa184c756f8
Brian Yu <brian@byu.io>
2019-03-02 22:30:25 -0500

    Remove extraneous files

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ import json
 import os
 from google.cloud import vision
 from google.cloud import storage
+from google.cloud import language
 
 # ================== FUNCTIONS ===========================
 
