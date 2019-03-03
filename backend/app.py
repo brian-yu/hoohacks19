bff3accac534a7f68942543371f159a54ad14bbc
Brian Yu <brian@byu.io>
2019-03-02 20:13:03 -0500

    Restart

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from flask import Flask
- 
+import requests
+import base64
+import json
+import os
+
+
 app = Flask(__name__)
  
  
 @app.route('/')
 def hello_whale():
     return 'Hey guys!'
+
+
+@app.route('/upload')
+def upload():
+	return ''
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
