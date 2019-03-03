855c363deee2b92659f4ec4ab677e1feafd5a90b
Rashid Lasker <rashidlasker@gmail.com>
2019-03-02 22:55:22 -0500

    add neo4j

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,10 @@ import requests
 import base64
 import json
 import os
-
+from py2neo import Graph
 
 app = Flask(__name__)
- 
+graph = Graph("bolt://db-api:7687", auth=("neo4j","reinform"))
  
 @app.route('/')
 def hello_whale():
@@ -16,6 +16,11 @@ def hello_whale():
 @app.route('/upload')
 def upload():
 	return ''
+
+@app.route("/graph")
+def get_graph():
+    results = graph.run("MATCH (people:Person) RETURN people.name LIMIT 10").to_table()
+    return str(results)
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
