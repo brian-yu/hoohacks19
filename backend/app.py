e3400083042552968156bdfc42ebe850f51a7202
aaron-gu <ag5ym@virginia.edu>
2019-03-02 20:20:42 -0500

    flask camera

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,14 @@ from flask import Flask
  
 app = Flask(__name__)
  
- 
+
 @app.route('/')
 def hello_whale():
     return 'Hey guys!'
+
+@app.route('/camera')
+def camera():
+    pass
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Camera from 'react-html5-camera-photo';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    fetch('backend:5000')
+      .then(function(response) {
+        console.log(response.body);
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+      console.log("dsfaasfsa")
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Camera 
+        onTakePhoto="send_photo"
+        onCameraError="camera_error"
+      />
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
