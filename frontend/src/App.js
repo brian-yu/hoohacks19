c3e14ec4f12d40afc3a949e399e6a8b4be47a167
Rashid Lasker <rashidlasker@gmail.com>
2019-03-03 04:28:06 -0500

    lmao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,44 +4,16 @@ import MobileView from './MobileView/MobileView.js';
 import { Button, Navbar, NavDropdown, Nav, Form, FormControl, ButtonToolbar, Alert } from 'react-bootstrap';
 
 class App extends Component {
-
-  alerts = () => {
-    return (
-    [
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-      ].map((variant, idx) => (
-        <Alert key={idx} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))
-      )
-  }
   
   render() {
     return (
       <div className="App">
 
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Note Nodes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -49,11 +21,6 @@ class App extends Component {
           </Form>
         </Navbar.Collapse>
       </Navbar>
-
-      <Button variant="secondary" size="lg"/>
-      
-      <this.alerts />
-
 
        {/* <MobileView/>*/}
       </div>
