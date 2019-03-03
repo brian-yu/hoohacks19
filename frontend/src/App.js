66a57da9a945f241b8737279386c59a5c354db05
Brian Yu <brian@byu.io>
2019-03-03 04:19:01 -0500

    Comment out stufF

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+{/*
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -52,10 +52,10 @@ class App extends Component {
 
       <Button variant="secondary" size="lg"/>
       
-      <this.alerts />
+      <this.alerts />*/}
 
 
-       {/* <MobileView/>*/}
+       <MobileView/>
       </div>
     
     );
